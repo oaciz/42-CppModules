5a638b23fe4aeb729f5ce1d64c3519c87ca370b0
Ömer Aciz <skorskyonline@gmail.com>
2024-03-11 02:07:13 +0300

    updated and finished

--- CPP07/ex01/Iter.hpp
+++ CPP07/ex01/Iter.hpp
@@ -8,4 +8,12 @@ void iter(T *array, size_t length, void (*func)(const T &element))
         func(array[i]);
     
 }
+
+template <typename T>
+void iter(T *array, size_t length, void (*func)(T &element))
+{
+    for (size_t i = 0; i < length; i++)
+        func(array[i]);
+    
+}
 #endif
